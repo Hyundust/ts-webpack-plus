476ac8d464c6b6214aadcdefc98a08647640e34e
Kostiantyn <improvise1337@gmail.com>
2023-06-03 23:59:02 +0200

    changes in ts config

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -4,6 +4,6 @@ import { Button } from "./Button"
 describe("classNames",()=>{
     test("should add button class",()=>{
         render(<Button>TEST</Button>);
-        expect(screen.getByText("TEST"))
+        expect(screen.getByText("TEST")).toBeInTheDocument
 });
 });
