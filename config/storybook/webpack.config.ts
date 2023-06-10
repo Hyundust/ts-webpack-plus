073f8e8bfdf28199a488fcc0871c4748d50c7c0c
Kostiantyn <improvise1337@gmail.com>
2023-06-10 19:53:47 +0200

    d

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,47 +1,32 @@
-import path from "path";
-import { BuildPath } from "../build/types/config";
-import  {buildCssLoader}  from "../build/loaders/buildCSSLoader";
-import type webpack from 'webpack'
+import webpack, { RuleSetRule } from 'webpack';
+import path from 'path';
+import { buildCssLoader } from '../build/loaders/buildCSSLoader';
+import { BuildPath } from '../build/types/config';
 
-export default ({ config }: { config: webpack.Configuration }) => {
-  const paths:BuildPath = {
-    entry: '',
-    build: '',
-    html: '',
-    src: path.resolve(__dirname,"..","..","src"),
-  }
-
-  // Add the src path to the list of directories that webpack should check for modules
-  if (config.resolve && config.resolve.modules) {
+export default ({ config }: {config: webpack.Configuration}) => {
+    const paths: BuildPath = {
+        build: '',
+        html: '',
+        entry: '',
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    };
     config.resolve.modules.push(paths.src);
-  }
+    config.resolve.extensions.push('.ts', '.tsx');
 
-  // Add TypeScript file extensions to the list of resolvable file extensions (if the extensions array exists)
-  if (config.resolve && config.resolve.extensions) {
-    config.resolve.extensions.push(".ts",".tsx");
-  }
-
-  if (config.module !== undefined && config.module.rules !== undefined) {
-    config.module.rules.push(buildCssLoader(true));
-  }
-    
-
-    // Modify the existing rule that loads svg files using url-loader to exclude .svg files
-  if (config.module && config.module.rules) {
-      config.module.rules = config.module.rules.map((rule: webpack.RuleSetRule | "...") => {
-        if (rule instanceof Object && rule.test && /svg/.test(rule.test.toString())) {
-          return { ...rule, exclude: /\.svg$/i };
+    // eslint-disable-next-line no-param-reassign
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
         }
+
         return rule;
-      });
+    });
 
-      config.module.rules.push({
+    config.module.rules.push({
         test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-    }
-  
+        use: ['@svgr/webpack'],
+    });
+    config.module.rules.push(buildCssLoader(true));
 
-  return config;
-
-  }
+    return config;
+};
